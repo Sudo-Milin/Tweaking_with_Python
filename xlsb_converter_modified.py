0db90b808cba612ed86317a0b46a5dbd5fdffb49
Milin Prajapati <69210892+Sudo-Milin@users.noreply.github.com>
2023-05-22 22:18:50 +0530

    Add files via upload

---
+++ xlsb_converter_modified.py
@@ -0,0 +1,42 @@
+# Created by: Ross Gibson
+
+import win32com.client as win32
+import os
+
+def getFiles(directory):
+    '''given a directory, will return list of all files in given directory'''
+    fileList = []  # place holder used for accumulation
+    if directory[-1] != '\\':  # check if path doesn't end with a back slash
+        directory = directory + '\\'  # add backslash to path
+    for item in os.scandir(directory):  # loop through each item
+        if item.is_file():  # check if item is a file
+            fileList.append(directory + item.name)  # append path and file name to accumulator list
+    return fileList  # return accumulated list
+
+def convertFiles(fileList):
+    '''given a list of file paths, will save .xls, .xlsx, .xlsm, or .csv files as .xlsb in same directory'''
+    for file in fileList:  # loop through each file in provided list
+        if os.path.splitext(file)[1] in ['.xls', '.xlsx', '.xlsm', '.csv']:  # check if excel or csv file
+            tgtPath = os.path.splitext(file)[0] + '.xlsb'  # sets target path as .xlsb file extension
+            xlApp = win32.Dispatch('Excel.Application')  # create Excel object
+            xlApp.Visible = False  # hide Excel window
+            xlApp.ScreenUpdating = False  # don't update Excel window (no window flashes)
+            xlApp.DisplayAlerts = False  # do not display alerts like updating links
+            try:
+                wb = xlApp.Workbooks.Open(Filename=file, ReadOnly=True)  # try opening the file in read-only mode
+            except:
+                print(f'Could not open {file}')  # print file if cannot be opened
+                continue
+            wb.SaveAs(Filename=tgtPath, FileFormat=50)  # save file as xlsb file format
+            wb.Close(False)  # closes Excel workbook without saving
+            xlApp.Quit()  # kill Excel process
+            print(f'Saved {tgtPath} from {file}')  # print confirmation
+
+def main():
+    '''given a windows formatted folder path, will convert all .xls, .xlsx, .xlsm, and .csv files in that directory
+    to .xlsb format'''
+    dir_path = str(input('Paste the path of the folder: '))
+    fileList = getFiles(dir_path)  # get list of files in provided directory
+    convertFiles(fileList)  # convert Excel/CSV files in file list
+
+main()
